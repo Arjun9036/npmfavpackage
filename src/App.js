c58a5e7447f8a1d73f735bbab7325d2afa21b17b
Arjun Goyal <155140221+Arjun9036@users.noreply.github.com>
2024-05-16 12:18:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import SearchNpmPackages from './Components/SearchNpmPackages';
 import ViewFavoritePackages from './Components/ViewFavoritePackages';
 import EditFavoritePackage from './Components/EditFavoritePackage';
